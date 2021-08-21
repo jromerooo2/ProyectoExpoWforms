b95224f00ff8d50c9353da0162df4385aaf866dd
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2021-08-20 18:59:04 -0600

    ....

--- SistemGestionBuses/frmUsuarios.cs
+++ SistemGestionBuses/frmUsuarios.cs
@@ -56,5 +56,35 @@ namespace SistemGestionBuses
         
 
     }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            EnvioDatos();
+        }
+        void EnvioDatos()
+        {
+
+            try
+            {
+                string user, apellido,password;
+                int id_empleado;
+
+                bool respuesta = objCond.EnviarDatosControlador();
+                if (respuesta == true)
+                {
+                    MessageBox.Show("Usuario registrado exitosamente", "Confirmación de ingreso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Usuario no pudo ser registrado", "Confirmación de ingreso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Oops!, ocurrió un error al registrar al empleado, consulte con el administrador del sistema.", "Error crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
