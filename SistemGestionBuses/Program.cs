b0f159b2f10f151cb1841a29b95be4b2aafec4c0
SantiMerino <santiago.merino2004@gmail.com>
2021-08-31 08:25:46 -0600

    login xd

--- SistemGestionBuses/Program.cs
+++ SistemGestionBuses/Program.cs
@@ -16,7 +16,7 @@ namespace SistemGestionBuses
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmIngresarViaje());
+            Application.Run(new frmLogin());
 
             //pasen siempre a frmLogin plis
         }
