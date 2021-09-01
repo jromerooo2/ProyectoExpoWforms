97229de488a272624da6ea6238881fb883e4efda
SantiMerino <santiago.merino2004@gmail.com>
2021-08-31 22:58:44 -0600

    frmIngresarUnidades

--- Controlador/ControladorTransporte.cs
+++ Controlador/ControladorTransporte.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using Modelo;
 
 namespace Controlador
 {
-    class ControladorTransporte
+    public class ControladorTransporte
     {
+
+        //Atributos
+        public static int id_unidad_transporte { get; set; }
+        public int id_marca { get; set; }
+        public int anio { get; set; }
+        public string VIN { get; set; }
+        public string capacidad { get; set; }
+        public int id_modelo { get; set; }
+        public string placa { get; set; }
+        public int id_tipo_placa { get; set; }
+        public int id_tipo_unidad { get; set;}
+        public string numero_chasis { get; set; }
+        public string numero_motor { get; set; }
+
+        //Constructor
+        ControladorTransporte(int pMarca, int pAnio, string pVIN, string pCapacidad, int pModelo, string pPlaca, int pTipoPlaca, int pTipoUnidad, string pNumeroChasis, string pNumeroMotor)
+        {
+            id_marca = pMarca;
+            anio = pAnio;
+            VIN = pVIN;
+            capacidad = pCapacidad;
+            id_modelo = pModelo;
+            placa = pPlaca;
+            id_tipo_placa = pTipoPlaca;
+            id_tipo_unidad = pTipoUnidad;
+            numero_chasis = pNumeroChasis;
+            numero_motor = pNumeroMotor;
+        }
+
+        public static DataTable ObtenerUnidadesController()
+        {
+            return ModeloTransporte.ObtenerUnidades();
+        }
+
+        //CMB
+        public static DataTable ObtenerTipoPlacasController()
+        {
+            return ModeloTransporte.ObtenerTipoPlacas();
+        }
+
+        public static DataTable ObtenerModelosController()
+        {
+            return ModeloTransporte.ObtenerModelos();
+        }
+
+        public static DataTable ObtenerMarcasController()
+        {
+            return ModeloTransporte.ObtenerMarcas();
+        }
+
+        public static DataTable ObtenerTipoUnidadController()
+        {
+            return ModeloTransporte.ObtenerTipoUnidad();
+        }
     }
 }
--- Modelo/ModeloTransporte.cs
+++ Modelo/ModeloTransporte.cs
@@ -3,10 +3,140 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Modelo
 {
-    class ModeloTransporte
+    public class ModeloTransporte
     {
+        //CMB de los tipos de unidades
+        public static DataTable ObtenerTipoUnidad()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_tipo_unidad;";
+                MySqlCommand cmdCliente = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdCliente);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+        public static DataTable ObtenerUnidades()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_unidad_transporte;";
+                MySqlCommand cmdUnidad = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdUnidad);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        public static DataTable ObtenerTipoPlacas()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_tipo_placa;";
+                MySqlCommand cmdUnidad = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdUnidad);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        public static DataTable ObtenerModelos()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_modelo;";
+                MySqlCommand cmdUnidad = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdUnidad);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        public static DataTable ObtenerMarcas()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_marca;";
+                MySqlCommand cmdUnidad = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdUnidad);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        //Cargar toda la tabla unidad de transporte
+        public static DataTable CargarUnidadTransporte()
+        {
+            DataTable data;
+            try
+            {
+                string instruccion = "SELECT * FROM tb_unidad_transporte";
+                MySqlCommand cmdCliente = new MySqlCommand(string.Format(instruccion), ModeloConexion.GetConnection());
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmdCliente);
+                data = new DataTable();
+                adp.Fill(data);
+                return data;
+            }
+            catch (Exception)
+            {
+                return data = null;
+            }
+        }
+
+        #region CRUD
+        public static bool IngresarUnidades(int pMarca, int pAnio, string pVIN, int pCapacidad, int pModelo, string pPlaca, int pTipoPlaca, int pTipoUnidad, string pNumeroMotor, string pNumeroChasis)
+        {
+            bool retorno = false;
+            try
+            {
+                int pEstadoUnidad = 3;
+                MySqlCommand cmdinsert = new MySqlCommand(string.Format("INSERT INTO tb_unidad_transporte(id_marca, anio, VIN, capacidad, id_modelo, placa, id_tipo_placa, id_tipo_unidad, id_estado_unidad, numero_motor, numero_chasis) VALUES ('{0}', '{1}', '{2}' , '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')", pMarca, pAnio, pVIN, pCapacidad, pModelo, pPlaca, pTipoPlaca, pTipoUnidad, pEstadoUnidad, pNumeroMotor, pNumeroChasis),ModeloConexion.GetConnection());
+                retorno = Convert.ToBoolean(cmdinsert.ExecuteNonQuery());
+                return retorno;
+            }
+            catch (Exception)
+            {
+                return retorno;
+            }
+        }
+
+        
+        #endregion
     }
 }
