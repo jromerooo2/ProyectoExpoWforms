ab6a506210b709741e4970c827b8a1d03136a375
SantiMerino <santiago.merino2004@gmail.com>
2021-09-07 16:47:00 -0600

    el album de drake es god

--- SistemGestionBuses/frmUsuarios.Designer.cs
+++ SistemGestionBuses/frmUsuarios.Designer.cs
@@ -35,6 +35,8 @@
             this.BtnActualizar = new System.Windows.Forms.Button();
             this.BtnAgregar = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.txtPin = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.txtId = new System.Windows.Forms.TextBox();
             this.cmbCargo = new System.Windows.Forms.ComboBox();
@@ -56,8 +58,6 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.txtPin = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
@@ -183,6 +183,25 @@
             this.panel2.Size = new System.Drawing.Size(634, 174);
             this.panel2.TabIndex = 74;
             // 
+            // txtPin
+            // 
+            this.txtPin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPin.Location = new System.Drawing.Point(333, 139);
+            this.txtPin.Name = "txtPin";
+            this.txtPin.Size = new System.Drawing.Size(213, 24);
+            this.txtPin.TabIndex = 71;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(206)))), ((int)(((byte)(237)))));
+            this.label8.Location = new System.Drawing.Point(333, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(206, 20);
+            this.label8.TabIndex = 69;
+            this.label8.Text = "Ingrese un Pin de seguridad";
+            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -283,6 +302,7 @@
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUser.Location = new System.Drawing.Point(30, 39);
             this.txtUser.Name = "txtUser";
+            this.txtUser.ShortcutsEnabled = false;
             this.txtUser.Size = new System.Drawing.Size(254, 24);
             this.txtUser.TabIndex = 45;
             // 
@@ -411,25 +431,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 82;
             this.pictureBox1.TabStop = false;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(206)))), ((int)(((byte)(237)))));
-            this.label8.Location = new System.Drawing.Point(333, 119);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(206, 20);
-            this.label8.TabIndex = 69;
-            this.label8.Text = "Ingrese un Pin de seguridad";
-            // 
-            // txtPin
-            // 
-            this.txtPin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPin.Location = new System.Drawing.Point(333, 139);
-            this.txtPin.Name = "txtPin";
-            this.txtPin.Size = new System.Drawing.Size(213, 24);
-            this.txtPin.TabIndex = 71;
             // 
             // frmUsuarios
             // 
