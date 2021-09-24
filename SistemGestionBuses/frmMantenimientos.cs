61c2c3e021de991384e477cb1e136925869deb04
davidghjg <82895581+davidghjg@users.noreply.github.com>
2021-09-23 21:17:54 -0600

    Inicio de CRUD Mantenimiento

--- SistemGestionBuses/frmMantenimientos.cs
+++ SistemGestionBuses/frmMantenimientos.cs
@@ -24,84 +24,12 @@ namespace SistemGestionBuses
         public ControladorMantenimiento objmaintenance;
         
         //DISPLAYING COMBO BOXES 
-        void Cargar_Marca()
-        {
-            CmbMarca.DataSource = ControladorMantenimiento.Load_Marca();
-            CmbMarca.ValueMember = "id_marca";
-            CmbMarca.DisplayMember = "marca";
-        }
-        void Cargar_Modelo()
-        {
-            CmbModelo.DataSource = ControladorMantenimiento.Load_Modelo();
-            CmbModelo.ValueMember = "id_modelo";
-            CmbModelo.DisplayMember = "modelo";
-        }
-        //INSERTING DATA PLATE
-        void CargarPlaca(int placa)
-        {
-            CmbPlaca.DataSource = ControladorMantenimiento.backing_PlateData(placa);
-            CmbPlaca.DisplayMember = "matricula";
-        }
 
         /*DATA INSERT*/
         //REGISTERING MAINTENANCE
-        void registering_maintenance()
-        {
-            try
-            {
-                string descripcion, fecha;
-                int id_unidad_transporte, monto_mantenimiento,
-                    ultimo_kilometraje, marca, modelo, placa;
-                //Sending registers
-                descripcion = TxtDescripcion.Text;
-                fecha = DtFecha.Text;
-                id_unidad_transporte = Convert.ToInt16(TxtID_unidad.Text);
-                monto_mantenimiento = Convert.ToInt16(TxtMonto.Text);
-                ultimo_kilometraje = Convert.ToInt16(TxtUltimoKilom.Text);
-                marca = Convert.ToInt16(CmbMarca.SelectedValue);
-                modelo = Convert.ToInt16(CmbModelo.SelectedValue);
-                placa = Convert.ToInt16(CmbPlaca.SelectedValue);
-                //Instace random object
-                objmaintenance = new ControladorMantenimiento(id_unidad_transporte, monto_mantenimiento,
-                                                            ultimo_kilometraje, descripcion, fecha);
-                bool answer = objmaintenance.backing_Maintenance();
-                if (answer == true)
-                {
-                    MessageBox.Show("El registro de mantenimiento se ha registrado con exito", 
-                                    "Proceso Completado",
-                                     MessageBoxButtons.OK,MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("El registro de mantenimiento no ha podido ser registrado",
-                                    "Proceso Completado",
-                                     MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Lo sentimos ಥ_ಥ, ha surgido un error externo al realizar " +
-                                "el registro del mantenimiento",
-                                    "Proceso Completado",
-                                     MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+        
 
-        private void BtnAdd_Click(object sender, EventArgs e)
-        {
-            registering_maintenance();
-            LoadDataGridView();
-            Cargar_Marca();
-            Cargar_Modelo();
-        }
 
-        private void frmMantenimientos_Load(object sender, EventArgs e, int placa)
-        {
-            Cargar_Marca();
-            Cargar_Modelo();
-            CargarPlaca(placa);
-            LoadDataGridView();
-        }
 
         void LoadDataGridView()
         {
@@ -109,105 +37,63 @@ namespace SistemGestionBuses
             DgvMaintenance.DataSource = DataGridView_useonly;
         }
 
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtDescripcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtMonto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtUltimoKilom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DtFecha_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtMonto_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
         /*DATA UPDATE*/
-        private void DgvMaintenance_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            int position = DgvMaintenance.CurrentRow.Index;
 
-            TxtID_mant.Text = DgvMaintenance[0, position].Value.ToString();
-            TxtID_unidad.Text = DgvMaintenance[1, position].Value.ToString();
-            TxtMonto.Text = DgvMaintenance[2, position].Value.ToString();
-            TxtUltimoKilom.Text = DgvMaintenance[3, position].Value.ToString();
-            TxtDescripcion.Text = DgvMaintenance[4, position].Value.ToString();
-            DtFecha.Text = DgvMaintenance[5, position].Value.ToString();
-        }
 
-        private void CmbMarca_Click(object sender, EventArgs e)
-        {
-            Cargar_Marca();
-        }
 
-        private void CmbModelo_Click(object sender, EventArgs e)
-        {
-            Cargar_Modelo();
-        }
 
-        private void CmbPlaca_Click(object sender, EventArgs e, int placa)
-        {
-            CargarPlaca(placa);
-        }
-
-        void ActualizarDatos()
-        {
-            try
-            {
-                string descripcion, fecha;
-                int id_unidad_transporte, monto_mantenimiento,
-                    ultimo_kilometraje, marca, modelo, placa;
-                //Sending the register values to textboxes
-                descripcion = TxtDescripcion.Text;
-                fecha = DtFecha.Text;
-                id_unidad_transporte = Convert.ToInt16(TxtID_unidad.Text);
-                monto_mantenimiento = Convert.ToInt16(TxtMonto.Text);
-                ultimo_kilometraje = Convert.ToInt16(TxtUltimoKilom.Text);
-                marca = Convert.ToInt16(CmbMarca.SelectedValue);
-                modelo = Convert.ToInt16(CmbModelo.SelectedValue);
-                placa = Convert.ToInt16(CmbPlaca.SelectedValue);
-                //Instance objempleado
-                ControladorMantenimiento.id_mantenimiento = Convert.ToInt16(TxtID_mant.Text);
-                objmaintenance = new ControladorMantenimiento(id_unidad_transporte, monto_mantenimiento, ultimo_kilometraje, descripcion, fecha);
-                bool respuesta = objmaintenance.Maintenance_Update();
-                if (respuesta == true)
-                {
-                    MessageBox.Show("El registro se ha actualizado exitosamente.", "Confirmación de actualización",
-                                        MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("No se ha podido actualizar el registro.", "Confirmación de actualización",
-                                       MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Vayaa ಥ_ಥ, se produjo un error externo al actualizar el regsitro.", "Error Crítico",
-                                       MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        private void BtnUpdate_Click(object sender, EventArgs e)
-        {
-            LoadDataGridView();
-            ActualizarDatos();
-        }
 
         /*DATA DELETE*/
-        void Delete_Mantenimiento()
-        {
-            ControladorMantenimiento.id_mantenimiento = Convert.ToInt16(TxtID_mant.Text);
-            bool delete = ControladorMantenimiento.Delete_Maintenance();
-            if (delete == true)
-            {
-                MessageBox.Show("El registro ha sido eliminado exitosamente", 
-                                    "Confirmación de eliminación",
-                                    MessageBoxButtons.OK,MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("El registro no ha podido ser eliminado",
-                                    "Confirmación de eliminación",
-                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
 
-        private void BtnDelete_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("¿Estas seguro de eliminar el registro de mantenimiento del vehículos con placas:" +CmbPlaca.Text+ "?", "Confirmación de eliminación",
-                                    MessageBoxButtons.YesNo,MessageBoxIcon.Question)== DialogResult.Yes)
-            {
-                Delete_Mantenimiento();
-                LoadDataGridView();
-            }
-        }
+
+
     }
-}
+
+ 
+    }
+
