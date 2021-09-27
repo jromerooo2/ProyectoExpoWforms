c388fbf5ad611f99e54d6d51ec19af265a86db88
kevinrivasd <kevinrivasd04@gmail.com>
2021-09-27 09:51:43 -0600

    .

--- SistemGestionBuses/frmRecuperar.Designer.cs
+++ SistemGestionBuses/frmRecuperar.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRecuperar));
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -43,8 +44,19 @@
             this.btnNueva = new System.Windows.Forms.Button();
             this.btnRecuPIN = new System.Windows.Forms.Button();
             this.txtUser = new System.Windows.Forms.TextBox();
+            this.panelBar = new System.Windows.Forms.Panel();
+            this.bunifuImageButton5 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton4 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton3 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox2
@@ -54,7 +66,7 @@
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(667, 618);
+            this.pictureBox2.Size = new System.Drawing.Size(667, 657);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -63,7 +75,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(279, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(279, 110);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(109, 73);
@@ -75,7 +87,7 @@
             // 
             this.btnMetodo2.BackColor = System.Drawing.Color.Black;
             this.btnMetodo2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMetodo2.Location = new System.Drawing.Point(279, 227);
+            this.btnMetodo2.Location = new System.Drawing.Point(279, 307);
             this.btnMetodo2.Margin = new System.Windows.Forms.Padding(2);
             this.btnMetodo2.Name = "btnMetodo2";
             this.btnMetodo2.Size = new System.Drawing.Size(109, 51);
@@ -88,7 +100,7 @@
             // 
             this.btnMetodo1.BackColor = System.Drawing.Color.Black;
             this.btnMetodo1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMetodo1.Location = new System.Drawing.Point(159, 227);
+            this.btnMetodo1.Location = new System.Drawing.Point(159, 307);
             this.btnMetodo1.Margin = new System.Windows.Forms.Padding(2);
             this.btnMetodo1.Name = "btnMetodo1";
             this.btnMetodo1.Size = new System.Drawing.Size(109, 51);
@@ -101,7 +113,7 @@
             // 
             this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(155, 127);
+            this.label2.Location = new System.Drawing.Point(155, 185);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(356, 58);
@@ -115,7 +127,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(213, 20);
+            this.label1.Location = new System.Drawing.Point(216, 61);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 28);
@@ -124,7 +136,7 @@
             // 
             // txtCode
             // 
-            this.txtCode.Location = new System.Drawing.Point(221, 350);
+            this.txtCode.Location = new System.Drawing.Point(222, 418);
             this.txtCode.Margin = new System.Windows.Forms.Padding(2);
             this.txtCode.Name = "txtCode";
             this.txtCode.ShortcutsEnabled = false;
@@ -136,7 +148,7 @@
             this.lblIngresoCodigo.AutoSize = true;
             this.lblIngresoCodigo.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblIngresoCodigo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblIngresoCodigo.Location = new System.Drawing.Point(123, 304);
+            this.lblIngresoCodigo.Location = new System.Drawing.Point(129, 373);
             this.lblIngresoCodigo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblIngresoCodigo.Name = "lblIngresoCodigo";
             this.lblIngresoCodigo.Size = new System.Drawing.Size(420, 28);
@@ -147,7 +159,7 @@
             // 
             this.btnConfirmar.BackColor = System.Drawing.Color.Black;
             this.btnConfirmar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnConfirmar.Location = new System.Drawing.Point(279, 385);
+            this.btnConfirmar.Location = new System.Drawing.Point(279, 454);
             this.btnConfirmar.Margin = new System.Windows.Forms.Padding(2);
             this.btnConfirmar.Name = "btnConfirmar";
             this.btnConfirmar.Size = new System.Drawing.Size(109, 51);
@@ -161,7 +173,7 @@
             this.lblNuevaContra.AutoSize = true;
             this.lblNuevaContra.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNuevaContra.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblNuevaContra.Location = new System.Drawing.Point(224, 456);
+            this.lblNuevaContra.Location = new System.Drawing.Point(216, 518);
             this.lblNuevaContra.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNuevaContra.Name = "lblNuevaContra";
             this.lblNuevaContra.Size = new System.Drawing.Size(230, 28);
@@ -170,7 +182,7 @@
             // 
             // txtNueva
             // 
-            this.txtNueva.Location = new System.Drawing.Point(221, 503);
+            this.txtNueva.Location = new System.Drawing.Point(221, 562);
             this.txtNueva.Margin = new System.Windows.Forms.Padding(2);
             this.txtNueva.Name = "txtNueva";
             this.txtNueva.ShortcutsEnabled = false;
@@ -181,7 +193,7 @@
             // 
             this.btnNueva.BackColor = System.Drawing.Color.Black;
             this.btnNueva.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnNueva.Location = new System.Drawing.Point(279, 540);
+            this.btnNueva.Location = new System.Drawing.Point(279, 595);
             this.btnNueva.Margin = new System.Windows.Forms.Padding(2);
             this.btnNueva.Name = "btnNueva";
             this.btnNueva.Size = new System.Drawing.Size(109, 51);
@@ -194,7 +206,7 @@
             // 
             this.btnRecuPIN.BackColor = System.Drawing.Color.Black;
             this.btnRecuPIN.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnRecuPIN.Location = new System.Drawing.Point(402, 227);
+            this.btnRecuPIN.Location = new System.Drawing.Point(402, 307);
             this.btnRecuPIN.Margin = new System.Windows.Forms.Padding(2);
             this.btnRecuPIN.Name = "btnRecuPIN";
             this.btnRecuPIN.Size = new System.Drawing.Size(109, 51);
@@ -205,39 +217,126 @@
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(221, 186);
+            this.txtUser.Location = new System.Drawing.Point(221, 265);
             this.txtUser.Margin = new System.Windows.Forms.Padding(2);
             this.txtUser.Name = "txtUser";
             this.txtUser.ShortcutsEnabled = false;
             this.txtUser.Size = new System.Drawing.Size(224, 20);
             this.txtUser.TabIndex = 11;
             // 
+            // panelBar
+            // 
+            this.panelBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(26)))), ((int)(((byte)(43)))));
+            this.panelBar.Controls.Add(this.bunifuImageButton5);
+            this.panelBar.Controls.Add(this.bunifuImageButton4);
+            this.panelBar.Controls.Add(this.bunifuImageButton3);
+            this.panelBar.Controls.Add(this.bunifuImageButton1);
+            this.panelBar.Location = new System.Drawing.Point(0, 0);
+            this.panelBar.Name = "panelBar";
+            this.panelBar.Size = new System.Drawing.Size(667, 50);
+            this.panelBar.TabIndex = 107;
+            // 
+            // bunifuImageButton5
+            // 
+            this.bunifuImageButton5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuImageButton5.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton5.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton5.Image")));
+            this.bunifuImageButton5.ImageActive = null;
+            this.bunifuImageButton5.Location = new System.Drawing.Point(546, 9);
+            this.bunifuImageButton5.Name = "bunifuImageButton5";
+            this.bunifuImageButton5.Size = new System.Drawing.Size(25, 31);
+            this.bunifuImageButton5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton5.TabIndex = 98;
+            this.bunifuImageButton5.TabStop = false;
+            this.bunifuImageButton5.Zoom = 10;
+            this.bunifuImageButton5.Click += new System.EventHandler(this.bunifuImageButton5_Click);
+            // 
+            // bunifuImageButton4
+            // 
+            this.bunifuImageButton4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuImageButton4.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton4.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton4.Image")));
+            this.bunifuImageButton4.ImageActive = null;
+            this.bunifuImageButton4.Location = new System.Drawing.Point(577, 9);
+            this.bunifuImageButton4.Name = "bunifuImageButton4";
+            this.bunifuImageButton4.Size = new System.Drawing.Size(35, 31);
+            this.bunifuImageButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton4.TabIndex = 97;
+            this.bunifuImageButton4.TabStop = false;
+            this.bunifuImageButton4.Zoom = 10;
+            this.bunifuImageButton4.Click += new System.EventHandler(this.bunifuImageButton4_Click);
+            // 
+            // bunifuImageButton3
+            // 
+            this.bunifuImageButton3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
+            this.bunifuImageButton3.ImageActive = null;
+            this.bunifuImageButton3.Location = new System.Drawing.Point(618, 9);
+            this.bunifuImageButton3.Name = "bunifuImageButton3";
+            this.bunifuImageButton3.Size = new System.Drawing.Size(27, 31);
+            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton3.TabIndex = 96;
+            this.bunifuImageButton3.TabStop = false;
+            this.bunifuImageButton3.Zoom = 10;
+            this.bunifuImageButton3.Click += new System.EventHandler(this.bunifuImageButton3_Click);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(18, 9);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(28, 30);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 94;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
             // frmRecuperar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(667, 618);
-            this.Controls.Add(this.btnRecuPIN);
+            this.ClientSize = new System.Drawing.Size(667, 657);
             this.Controls.Add(this.btnNueva);
             this.Controls.Add(this.txtNueva);
             this.Controls.Add(this.lblNuevaContra);
             this.Controls.Add(this.btnConfirmar);
-            this.Controls.Add(this.lblIngresoCodigo);
             this.Controls.Add(this.txtCode);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.btnMetodo2);
+            this.Controls.Add(this.lblIngresoCodigo);
             this.Controls.Add(this.btnMetodo1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRecuPIN);
+            this.Controls.Add(this.btnMetodo2);
             this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelBar);
             this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmRecuperar";
             this.Text = "Recupera Tu Cuenta";
             this.Load += new System.EventHandler(this.frmRecuperar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -259,5 +358,11 @@
         private System.Windows.Forms.Button btnNueva;
         private System.Windows.Forms.Button btnRecuPIN;
         private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Panel panelBar;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton5;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton4;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton3;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
     }
 }
