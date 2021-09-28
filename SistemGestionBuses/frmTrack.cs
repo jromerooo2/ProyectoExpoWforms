4bf2467da0f9acd15f9be781d083c4c0a08ca77b
davidghjg <82895581+davidghjg@users.noreply.github.com>
2021-09-28 12:36:26 -0600

    Revert ".."
    This reverts commit 2701c490fc79049276e7eb0dd2326d861be1ae74, reversing
    changes made to d829722b23b7829c8d3f20119d8467010184d7ec.

--- SistemGestionBuses/frmTrack.cs
+++ SistemGestionBuses/frmTrack.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebSocketSharp;
+//using WebSocketSharp;
 using System.Windows.Forms;
 
 namespace SistemGestionBuses
@@ -20,18 +20,7 @@ namespace SistemGestionBuses
 
         private void frmTrack_Load(object sender, EventArgs e)
         {
-            using (WebSocket ws = new WebSocket("ws://frequent-cultured-timbale.glitch.me/"))
-            {
-                ws.Connect();
-                ws.OnMessage += Ws_OnMessage;
-                ws.Send("Hello Server");
-            }        
-
-        }
-
-        private void Ws_OnMessage(object sender, MessageEventArgs e)
-        {
-            MessageBox.Show("Received from server" + e.Data);
+            //WebSocket ws = new WebSocket("ws://simple-websocket-server-echo.glitch.me/");
         }
     }
 }
