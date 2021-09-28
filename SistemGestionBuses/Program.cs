d3b67ea71c8f433cef55e8115ce2a000388004a5
SantiMerino <santiago.merino2004@gmail.com>
2021-09-28 11:08:58 -0600

    Ni me acuerdo que es esto :-1:

--- SistemGestionBuses/Program.cs
+++ SistemGestionBuses/Program.cs
@@ -16,7 +16,7 @@ namespace SistemGestionBuses
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMantenimientos());
+            Application.Run(new frmIngresoConductores(5));
             //pasen siempre a frmLogin plis - Aviso Parroquial
             //new frmConfig(id_cargo, "username", id_usuario)
         }
