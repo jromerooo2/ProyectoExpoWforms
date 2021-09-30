45aa868528d1f790ba2462fee73aef6ace29f3aa
SantiMerino <santiago.merino2004@gmail.com>
2021-09-29 23:07:04 -0600

    fixing conflict :angry:

--- SistemGestionBuses/Program.cs
+++ SistemGestionBuses/Program.cs
@@ -16,19 +16,7 @@ namespace SistemGestionBuses
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             Application.Run(new frmLogin());
-=======
-            Application.Run(new frmIngresoConductores(5));
->>>>>>> parent of 380952f (Merge branch 'dev' of https://github.com/jromerooo2/ProyectoExpoWforms into dev)
-=======
-            Application.Run(new frmTrack());
->>>>>>> 740e2659e2ada6c202d1a1785efbf67513930a9e
-=======
-            Application.Run(new frmLogin());
->>>>>>> parent of d829722 (..)
             //pasen siempre a frmLogin plis - Aviso Parroquial
             //new frmConfig(id_cargo, "username", id_usuario)
         }
